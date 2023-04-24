df5f3f9b5ede4e5b4fc00192705f8e00aa263b4e
Mikolaj Najda <najda.mikolaj00@gmail.com>
2023-04-24 11:09:03 +0200

    TSPBB file updated and Time test file already made

--- TSPBB.py
+++ TSPBB.py
@@ -5,13 +5,15 @@
 import numpy as np
 import math
 
-def tsp_bnb(cities, upper_bound=math.inf):
+def tsp_bnb(cities):
     """
-
+    Travelling Salesman Problem, using Branch and Bound method.
     """
+    upper_bound = calculate_upper_bound(cities)
     n = len(cities)
     best_tour = None
     stack = [(0, [0])]
+
     while stack:
         (cost, path) = stack.pop()
         if len(path) == n:
@@ -21,16 +23,39 @@ def tsp_bnb(cities, upper_bound=math.inf):
             for city in range(n):
                 if city not in path:
                     new_cost = cost + cities[path[-1], city]
-                    lower_bound = sum(np.min(cities, axis=1)) + sum(np.min(cities, axis=0)[np.array(list(set(range(n)) - set(path)))]) + cities[path[-1], city]
+                    lower_bound = calculate_lower_bound(cities, n, path, city)
                     if lower_bound < best_tour[0] if best_tour is not None else math.inf and new_cost < upper_bound:
                         stack.append((new_cost, path + [city]))
+        
+    cost += cities[path[-1], 0]
     return best_tour
 
+def calculate_lower_bound(cities, n, path, city):
+    """
+    Lower bound calculated using nearest neighbor heuristic,
+    the idea is to always choose the nearest unvisited city as the next city to visit.
+    """
+    return sum(np.min(cities, axis=1)) + sum(np.min(cities, axis=0)[np.array(list(set(range(n)) - set(path)))]) + cities[path[-1], city]
 
-# Example usage
-cities = np.array([[0, 10, 15, 20],
-                   [10, 0, 35, 25],
-                   [15, 35, 0, 30],
-                   [20, 25, 30, 0]])
-best_tour = tsp_bnb(cities)
-print("Best tour cost:", best_tour)
+def calculate_upper_bound(cities):
+    """
+    Upper bound calculated using nearest neighbor heuristic,
+    the idea is to always choose the nearest unvisited city as the next city to visit.
+
+    We need to find a tour that visits all cities once and returns to the starting city with the lowest possible cost.
+    """
+    n = len(cities)
+    unvisited = set(range(1, n))
+    current_city = 0
+    tour = [0]
+
+    while unvisited:
+        next_city = min(unvisited, key=lambda city: cities[current_city][city])
+        unvisited.remove(next_city)
+        tour.append(next_city)
+
+        current_city = next_city
+
+    tour.append(0)
+    cost = sum(cities[tour[i]][tour[i+1]] for i in range(n))
+    return cost
